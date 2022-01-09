32035afae4ec1bb30bd6a1dd00253f2081d84f06
eugenekalib <71548762+eugenekalib@users.noreply.github.com>
2022-01-09 16:06:55 +0300

    Delete jquery.js

--- jquery.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    $jsonObj = 0;
-    $.getJSON(' https://swapi.dev/api/people/?format=json', function(data) {
-        //data is the JSON string
-        $jsonObj = JSON.stringify(data);
-        console.log($jsonObj);
-    });
-});
